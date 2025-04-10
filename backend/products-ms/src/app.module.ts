32e6e272caa6e84d7152f16a53ebe52ef1eac4d9
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-04-09 21:03:08 -0300

    WIP: products-ms workflow-2

--- backend/products-ms/src/app.module.ts
+++ backend/products-ms/src/app.module.ts
@@ -22,7 +22,7 @@ import { User } from './modules/users/entities/user.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: envs.postgresHost || 'localhost',
-      port: parseInt(envs.postgresContainerPort, 10) || 5432,
+      port: 5432,
       username: envs.postgresUser || 'your_username',
       password: envs.postgresPassword || 'your_password',
       database: envs.postgresDb || 'your_database',
